9fd0824a266de85dda51232b0ee35d7f6c9766bd
Stéphane ZUCATTI <stephane.zucatti@me.com>
2023-06-16 10:30:50 +0200

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.langs(
-    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
     title varchar(255),
     flag varchar(2),
     aka varchar(2),
