f0a3197237d28eba19fbacb4ebecece1497d21dc
Stéphane ZUCATTI <stephane.zucatti@cerema.fr>
2023-11-28 20:27:27 +0100

    update

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,5 @@
 CREATE TABLE public.langs(
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
-    title varchar(255),
-    flag varchar(2),
-    aka varchar(2),
+    id bigserial NOT NULL PRIMARY KEY,
     createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
